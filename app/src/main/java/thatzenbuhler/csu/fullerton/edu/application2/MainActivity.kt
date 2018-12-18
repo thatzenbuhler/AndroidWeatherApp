097a67b4ae0bad3583383102b3300f44c845a146
thatzenbuhler <t.hatzenbuhler@gmail.com>
2018-12-18 12:12:11 -0800

    add json class and moshi gradle

--- app/src/main/java/thatzenbuhler/csu/fullerton/edu/application2/MainActivity.kt
+++ app/src/main/java/thatzenbuhler/csu/fullerton/edu/application2/MainActivity.kt
@@ -2,11 +2,18 @@ package thatzenbuhler.csu.fullerton.edu.application2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: get this to work fam
+        val jsondata = URL("http://api.openweathermap.org/data/2.5/weather?q=Fullerton,us&APPID=215f8ff3eab42cf87ccf1a8784c50456").readText()
+
+        //TODO: initialize Moshi to process json
+
     }
 }
---
+++ app/src/main/java/thatzenbuhler/csu/fullerton/edu/application2/WeatherData.kt
@@ -0,0 +1,54 @@
+package thatzenbuhler.csu.fullerton.edu.application2
+
+data class WeatherData(
+    val base: String,
+    val clouds: Clouds,
+    val cod: Int,
+    val coord: Coord,
+    val dt: Int,
+    val id: Int,
+    val main: Main,
+    val name: String,
+    val sys: Sys,
+    val visibility: Int,
+    val weather: List<Weather>,
+    val wind: Wind
+)
+
+data class Main(
+    val humidity: Int,
+    val pressure: Int,
+    val temp: Double,
+    val temp_max: Double,
+    val temp_min: Double
+)
+
+data class Weather(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
+
+data class Sys(
+    val country: String,
+    val id: Int,
+    val message: Double,
+    val sunrise: Int,
+    val sunset: Int,
+    val type: Int
+)
+
+data class Wind(
+    val deg: Double,
+    val speed: Double
+)
+
+data class Clouds(
+    val all: Int
+)
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
