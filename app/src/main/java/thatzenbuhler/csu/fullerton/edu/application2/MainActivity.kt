2fe6d49917c2fa729454fa2b6e4e6c31bb5baba3
thatzenbuhler <t.hatzenbuhler@gmail.com>
2018-12-19 17:20:47 -0800

    now working!

--- app/src/main/java/thatzenbuhler/csu/fullerton/edu/application2/MainActivity.kt
+++ app/src/main/java/thatzenbuhler/csu/fullerton/edu/application2/MainActivity.kt
@@ -2,7 +2,12 @@ package thatzenbuhler.csu.fullerton.edu.application2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.net.URL
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO: get this to work fam
-        val jsondata = URL("http://api.openweathermap.org/data/2.5/weather?q=Fullerton,us&APPID=215f8ff3eab42cf87ccf1a8784c50456").readText()
+        // Uses Volley to send a request for weather data as a string, in JSON format
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=Fullerton,us&APPID=215f8ff3eab42cf87ccf1a8784c50456"
 
-        //TODO: initialize Moshi to process json
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                processData(response)
+            },
+            Response.ErrorListener { temp.text = "Can't connect!" })
+        queue.add(stringRequest)
+    }
 
+    fun processData(response: String){
+        // Uses Klaxon to automatically parse the JSON and fill the WeatherData dataclass
+        val data = Klaxon()
+            .parse<WeatherData>(response)
+        val result : WeatherData = data!!
+
+        // Update text fields to reflect data
+        description.text = result.weather[0].description.capitalize()
+        temp.text = (((result.main.temp - 273.15) * 9/5) + 32).toString() + " °F"
+        humidity.text = result.main.humidity.toString() + "%"
+        wind.text = result.wind.deg.toString() + "° at " + result.wind.speed.toString() + " m/s"
     }
 }
